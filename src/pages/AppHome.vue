a47e6c3bc72729cb4dbe6e7e4869a2d37a40f369
alessandroamormino <amormino.alessandro97@gmail.com>
2023-06-01 17:42:07 +0200

    feat: changed home layout

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -10,10 +10,25 @@ export default {
 </script>
 
 <template>
-  <div class="container">
-    <h1>Vini, Vitigni, Cantine</h1>
+  <div class="container p-5 text-center">
+    <h1>Vini</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, quam vitae. Iste incidunt repudiandae fugiat itaque placeat iusto ipsum, laboriosam, sunt sit aut animi, dolore quis quidem debitis eaque voluptatum.</p>
+
+    <h2>Cantine</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam velit vero, explicabo rem facere, ratione quod hic quo quas in aperiam? Quisquam, non sunt. Temporibus sequi nostrum rerum minima incidunt.</p>
+
+    <h3>Vitigni</h3>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae quidem laborum maxime consequuntur? Nemo dolores ab id culpa est harum, error voluptate nisi eaque atque, quisquam, tempora quae expedita veniam.</p>
+
+    <h5 class="mt-5">Clicca nella barra di navigazione per i dettagli</h5>
   </div>
 </template>
 
 <style lang="scss">
+h1{
+  margin-bottom: 1em;
+}
+p{
+  font-size: 1.3em;
+}
 </style>
