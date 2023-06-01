6ce4f6605ab9e470e1cd8bd51cf230491e45dcd6
alessandroamormino <amormino.alessandro97@gmail.com>
2023-06-01 17:22:37 +0200

    style: singlewine

--- src/pages/SingleWine.vue
+++ src/pages/SingleWine.vue
@@ -22,13 +22,59 @@ export default{
 </script>
 <template>
   <div class="container py-4">
-    <h1>Vino: {{ wine.name }}</h1>
-    <h6 class="card-subtitle mb-2">{{ wine.type }}</h6>
-    <p class="card-text">{{ wine.year }} - {{ wine.winery?.name }}</p>
+    <div class="title">
+      <h1>Vino: {{ wine.name }}</h1>
+      <div class="wine-color" :class="wine.color"></div>
+    </div>
+    <h6 class="card-subtitle mb-2">Tipologia: {{ wine.type }}</h6>
+    <p class="card-text annata">{{ wine.year }} - {{ wine.winery?.name }}</p>
     <p v-if="wine.vineyards?.length > 0">Vitigni: <span v-for="vineyard in wine.vineyards">{{ vineyard.name }}</span></p>
     <p>Gradazione: {{ wine.gradation }}%</p>
     <p>Estratto: {{ wine.extract }}g/l</p>
     <p>Acidità: {{ wine.acidity }}g/l</p>
+    <p>Colore: {{ wine.color }}</p>
+
+    <router-link :to="{name: 'wines'}" class="btn btn-secondary">Torna alla lista dei vini</router-link>
   </div>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h1{
+  margin-bottom: 1em;
+}
+.title{
+  display: flex;
+  gap: 1em;
+}
+.wine-color{
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
+.annata{
+  font-size: .9em;
+  font-style: italic;
+}
+
+.rosso {
+    background-color: #900c3f;
+    color: white;
+    text-transform: capitalize;
+
+    &:hover {
+        color: white;
+    }
+}
+
+.bianco {
+    background-color: antiquewhite;
+    text-transform: capitalize;
+    color: #000;
+}
+
+.rosè {
+    background-color: #ffd7d782;
+    text-transform: capitalize;
+    color: #000;
+}
+
+</style>
