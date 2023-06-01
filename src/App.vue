f4c479700fe9dbe1c6981ef5f97286699feaba98
alessandroamormino <amormino.alessandro97@gmail.com>
2023-06-01 15:44:08 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import axios from 'axios';
+export default {
+  data(){
+    return {
+      URL: 'http://127.0.0.1:8000/api/wines',
+      wines: [],
+    }
+  }, 
+  methods: {
+    getWines(){
+      axios.get(this.URL).then(response => {
+        // console.log(response.data.results);
+        this.wines = response.data.results;
+      });
+    }
+  },
+  mounted(){
+    this.getWines();
+  },
+}
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss">
+</style>
