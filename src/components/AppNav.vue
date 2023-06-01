dc99f8705fab189ad3e3465437ca9f5d81a16782
alessandroamormino <amormino.alessandro97@gmail.com>
2023-06-01 17:44:09 +0200

    fix: navbar loro brand link

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -9,15 +9,12 @@ export default {
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <router-link :to="{name: 'home'}" class="navbar-brand">Home</router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <router-link :to="{name: 'home'}" class="nav-link">Home</router-link>
-        </li>
         <li class="nav-item">
           <!-- <a class="nav-link" href="#">Vini</a> -->
           <router-link :to="{name: 'wines'}" class="nav-link">Vini</router-link>
